9bc56d0b8f431ca00630aeff7dd3365bd6ee891b
fgierlinger <2966031+fgierlinger@users.noreply.github.com>
2020-11-21 20:35:06 +0100

    enable sync of nonempty directories

--- README.md
+++ README.md
@@ -12,6 +12,9 @@ All configuration is done via environment variables.
 | `GIT_SYNC_REPO` | | (Required) Repository that shall be cloned. If authentication is required, username and password can be included in the url (https://username:password@github.com/user/repo.git) |
 | `GIT_SYNC_SLEEP` | 1 | Time in seconds to sleep between the pulls |
 | `GIT_SYNC_PULL_OPTS` | -f | Options to pass to the `git pull` command |
+| `GIT_SYNC_BRANCH` | origin/master | Branch to checkout |
+| `GIT_SYNC_NOTEMPTY` | false | Per default git only clones into empty directories. If for whatever reason the directory is not empty, set this variable to true to still merge the git repository into the existing directory |
+| `GIT_SYNC_NOTEMPTY_OPTS` | --mixed | Options to pass to the `git reset` command, in case there is already content in the directory |
 
 ## Example use
 
--- sync.sh
+++ sync.sh
@@ -6,6 +6,9 @@ set -euo pipefail
 #GIT_SYNC_REPO
 GIT_SYNC_PULL_OPTS=${GIT_SYNC_PULL_OPTS:--f}
 GIT_SYNC_SLEEP=${GIT_SYNC_SLEEP:-1}
+GIT_SYNC_NOTEMPTY=${GIT_SYNC_NOTEMPTY:-false}
+GIT_SYNC_NOTEMPTY_OPTS=${GIT_SYNC_NOTEMPTY_OPTS:---mixed}
+GIT_SYNC_BRANCH=${GIT_SYNC_BRANCH:-master}
 
 function check_req_var() {
     if [ ! -v ${1} ]; then
@@ -29,7 +32,16 @@ fi
 
 # check if git directory already exists, if not, clone repo
 if [ ! -d "${GIT_SYNC_DIR}/.git" ]; then
-    git clone "${GIT_SYNC_REPO}" "${GIT_SYNC_DIR}"
+    if [ ${GIT_SYNC_NOTEMPTY} = false ]; then
+        git clone -b "${GIT_SYNC_BRANCH}" "${GIT_SYNC_REPO}" "${GIT_SYNC_DIR}"
+    else
+        git -C "${GIT_SYNC_DIR}" init
+        git -C "${GIT_SYNC_DIR}" remote add origin "${GIT_SYNC_REPO}"
+        git -C "${GIT_SYNC_DIR}" fetch
+        git -C "${GIT_SYNC_DIR}" reset "${GIT_SYNC_NOTEMPTY_OPTS}" "origin/${GIT_SYNC_BRANCH}"
+        git -C "${GIT_SYNC_DIR}" branch -u "origin/${GIT_SYNC_BRANCH}" "${GIT_SYNC_BRANCH}"
+
+    fi
 fi
 
 while true; do
